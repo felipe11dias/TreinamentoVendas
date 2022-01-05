8209a2025cbb71f4428686163daa7edcaecfca1f
felipe11dias <clame1997mac@hotmail.com>
2022-01-05 09:59:53 -0300

    # Autenticação via HTTP BASIC #

--- src/main/java/io/github/felipe11dias/config/SecurityConfig.java
+++ src/main/java/io/github/felipe11dias/config/SecurityConfig.java
@@ -32,7 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().disable()
 			.authorizeRequests()
 				.antMatchers("api/clientes/**")
-					.authenticated()
+					.hasAnyRole("USER", "ADMIN")
+				.antMatchers("api/pedidos/**")
+					.hasAnyRole("USER", "ADMIN")
+				.antMatchers("api/produtos/**")
+					.hasRole("ADMIN")
 			.and()
 				.formLogin();
 	}
